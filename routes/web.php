0c80423e55e2124f8c5c0a696795e469277a0888
Jose Seie <joseeugenioseie@hotmail.com>
2017-09-21 04:37:37 +0200

    Primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello',function (){
+//    return Auth::user()->teste();
+    return Auth::user()->hello();
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/profile/{slug}',[
+        'uses'=>'ProfileController@index',
+        'as' => 'profile'//Dando pseudonimo a rota.
+    ]);
+    Route::get('/profile/edit/profile',[
+        'uses'=>'ProfileController@edit',
+        'as' => 'profile.edit'//Dando   pseudonimo a rota.
+    ]);
+    Route::post('/profile/update/profile',[
+        'uses'=>'ProfileController@update',
+        'as' => 'profile.update'//Dando pseudonimo a rota.
+    ]);
+});
+
